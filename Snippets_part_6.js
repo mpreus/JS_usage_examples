396531ceb1a4d08b57856700a7a7eaaa3032de0e
Maciej Preus <mpreus@onet.eu>
2019-10-11 09:54:06 +0200

    Add more examples to Snippets_part_6.js

--- Snippets_part_6.js
+++ Snippets_part_6.js
@@ -4,7 +4,9 @@ W części 6
 [1] Jak dodać jedną tablicę (np. 'arr2') do innej (np. 'arr1') na jej końcu
 [2] Jak wyczyścić tablicę przypisaną do 'const'
 [3] Jak zwrócić elementy tablicy, które po zastosowaniu wobec każdego z nich określonej funkcji, istnieją jednocześnie (po zastosowaniu wobec każdego z nich określonej funkcji) w drugiej tablicy 
-
+[4] Jak sprawdzić, który ciąg znaków w określonym stringu jest najdłuższy
+[5] Jak sprawdzić czy dane słowo jest anagramem innego słowa (czyli zawierają te same litery w innej kolejności)
+[6] Jak sprawdzić czy wprowadzony argument jest iterowalny jak tablica
 */
 
 
@@ -44,6 +46,52 @@ const commonElemsByFn = (arrA, arrB, fn) => {
 commonElemsByFn([2.1, 1.2, 3.3], [2.3, 3.4, 4.1], Math.floor);
 // zwraca: [ 2.1, 3.3 ]
 // ponieważ zostaną zredukowane do (odpowiednio): 2 i 3, które to wartości po zredukowaniu występują w obu tablicach
+
+
+// [4] Jak sprawdzić, który ciąg znaków w określonym stringu jest najdłuższy
+let stringA = "mając dowolne zdanie lub dłuższy ciąg znaków chcemy znaleść najdłuższy wyraz i wiedzieć z ilu się składa znaków";
+// dla określonego stringu zwróć najdłuższy wyraz i jego liczbę znaków
+function longestWord(str) {
+  let words = str.split(' '), // string wprowadzany w argumencie funckji rozdzielamy na słowa (części) wg spacji między nimi
+      maxLength = 0,   // wartość początkowa największej długości
+      word = '';       // wartość początkowa najdłuższego ciągu znaków
+  for (let i = 0; i < words.length; i++) {
+// dla każdego stringu jeśli jest dłuższy od największej długości...
+    if (words[i].length > maxLength) {
+        word = words[i];      // ...przypisz go do najdłuższego ciągu znaków...
+        maxLength = words[i].length; // ...i wpisz jego długość jako największą długość
+    }
+  }
+  return [word, maxLength]; // wynik zwróć w formie dwuelementowej tablicy
+}
+longestWord(stringA); // zwraca: [ 'najdłuższy', 10 ]
+longestWord('niesamowity ciąg znaków'); // zwraca: [ 'niesamowity', 11 ]
+
+
+// [5] Jak sprawdzić czy dane słowo jest anagramem innego słowa (czyli zawierają te same litery w innej kolejności)
+// bierzemy pod uwagę dwa stringi (z założenia mają tę samą długość, ale najprawdopodobniej poprzestawiane litery)
+const ifIsAnagram = (str1, str2) => {
+  const normalize = str => str.toLowerCase()
+  .replace(/[^a-z0-9]/gi, '')
+  .split('')
+  .sort()
+  .join('');
+// powyżej do stałej 'normalize' przypisujemy wyrażenie funkcyjne, które wykonuje kolejno wskazane metody, gdzie pierwsza zamienia string na same małe litery, a druga zamienia na nieprzerwany ciąg znaków składających się z małych liter i cyfr
+  return normalize(str1) === normalize(str2);
+};
+ifIsAnagram('iceman', 'cinema'); // zwraca: true
+ifIsAnagram('laktoza', 'zalotka'); // zwraca: true
+ifIsAnagram('kino', 'kara'); // zwraca: false
+
+
+// [6] Jak sprawdzić czy wprowadzony argument jest iterowalny jak tablica
+const isArrayLike = obj => obj != null && typeof obj[Symbol.iterator] === 'function';
+isArrayLike('jakiś string'); // zwraca: true
+isArrayLike(NaN); // zwraca: false
+isArrayLike(undefined); // zwraca: false
+
+
+
 
 
 
